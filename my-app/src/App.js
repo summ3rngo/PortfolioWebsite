77c8ed915ed2f42271b74a1e8044e5effe336584
Summer Ngo <111797449+summ3rngo@users.noreply.github.com>
2023-06-12 10:00:01 -0700

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+import Nav from './components/header/Nav'
+import logo from './assets/logo.png'
+import headerimg from './assets/image-placeholder.jpg'
+import { useState } from 'react'
+import { useMediaQuery } from 'react-responsive';
+import React from 'react'
+import NavBarSmall from './components/header/NavBarSmall'
 
-function App() {
+
+const App = () => {
+  const [active, setactive] = useState('home');
+  const updateActiveLink = (value) => {
+
+    setactive(value);
+    console.log(value);
+    
+  }
+
+  const isSmallScreen = useMediaQuery({ maxWidth: 991 });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='overall-container'>
+      {isSmallScreen ? <NavBarSmall /> : <Nav logo={logo} updateActiveLink={updateActiveLink} active={active} />}
+    <Header  updateActiveLink={updateActiveLink} active={active} headerimg={headerimg}/>
+    <About />
+    <Skills />
+    <Projects />
+    <Contact />
     </div>
-  );
+  )
 }
 
 export default App;
